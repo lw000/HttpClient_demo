a640124fb8878c7eb32bceae8017c3332582b6d6
lw000 <373102227@qq.com>
2017-09-20 15:49:05 +0800

    update

--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,65 @@
 #include <stdio.h>
 #include <string>
 #include <unistd.h>
+#include <signal.h>
 
 #include "HttpClient.h"
 #include "HttpRequest.h"
 #include "HttpResponse.h"
 
-#include <log4z/log4z.h>
+#include "log4z.h"
 using namespace zsummer::log4z;
 
+#include <atomic>
+
+//static bool exit_loop = false;
+//
+//void sighandler(int) {
+//	exit_loop = true;
+//}
+
 int main(int argc, char ** argv) {
+	if (argc < 3)
+		return -1;
+
+//	::signal(SIGINT, sighandler);
+
 	ILog4zManager::getInstance()->start();
 
+	std::string s1(argv[1]);
+	std::string c1(argv[2]);
+	int c = atoi(c1.c_str());
+	int i = 1;
 	do {
 		HttpRequest *request = new HttpRequest();
-		request->setUrl("www.baidu.com");
+		request->setUrl(s1.c_str());
 		request->setRequestType(HttpRequest::Type::emGET);
 
-		request->setResponseCallback([](HttpResponse* response) {
-			long code = response->getResponseCode();
-			if (code == 200) {
-				std::string data = response->getResponseDataString();
-				LOGD(data);
-			} else {
-				const char* err = response->getErrorBuffer();
-				LOGD(err);
-			}
-		});
+		clock_t t = clock();
+		request->setResponseCallback(
+				[i, t](HttpResponse* response) {
+					clock_t t1 = clock();
+
+					double t2 = ((double)(t1)-t)/CLOCKS_PER_SEC;
+
+					long code = response->getResponseCode();
+					if (code == 200) {
+						std::string data = response->getResponseDataString();
+						LOGFMTD("[%d] [t: %f] [code: %ld] [data: %s]", i, t2, code, data.c_str());
+					} else {
+						const char* err = response->getErrorBuffer();
+						LOGFMTD("[%d] [t: %f] [code: %ld] [data: %s]", i,  t2, code, err);
+					}
+				});
 
 		HttpClient::getInstance()->send(request);
 //		HttpClient::getInstance()->sendImmediate(request);
 
-		sleep(1);
+//		usleep(100);
+		i++;
 
-	} while (1);
+	} while (i <= c);
 
 	getchar();
-
 	return 0;
 }
